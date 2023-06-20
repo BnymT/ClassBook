8089cf5d4efc793d180d94599161972c3b8ec3d9
BnymT <dragfung@gmail.com>
2023-06-20 23:09:38 +0300

    day30maps

--- src/main/java/day29collections/Queues02.java
+++ src/main/java/day29collections/Queues02.java
@@ -1,5 +1,7 @@
 package day29collections;
 
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
@@ -13,7 +15,7 @@ public class Queues02 {
         line.add("Cheese");
         line.add("Meat");
 
-        System.out.println(line);//[Cheese, Meat, Egg, Milk]
+        System.out.println(line); //[Cheese, Meat, Egg, Milk]
 
         /*
         Queue bir interface dir, bu yuzden constructor i yoktur, dolayisiyla object olustururken new keywordunden
@@ -22,6 +24,17 @@ public class Queues02 {
         Queue olustururken constructor olarak PriorityQueue kullanirsaniz elemanlari kendi belirleyeceginiz kurala gore siralama
         hakkiniz olur
          */
+
+        // Deque --> Double Ended Queue (Ikı uclu queue)
+        // Deque daki add(), get(), peek() vb... method lar first - end olarak iki uclu olarak bulunur.
+
+        Deque<String> d = new LinkedList<>();
+        d.add("Egg");
+        d.add("Milk");
+        d.add("Cheese");
+        d.add("Meat");
+
+        System.out.println(d); //[Cheese, Meat, Egg, Milk]
 
     }
 
